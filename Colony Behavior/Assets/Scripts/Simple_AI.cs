a8f312aa5724e904d48e8f5a9b65c6b95c965690
OvanHouten <olaf.vanhouten@student.uva.nl>
2018-11-22 17:51:43 +0100

    Created an Agent and updated bounds for better visibility

---
+++ Colony Behavior/Assets/Scripts/Simple_AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple_AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		print("Hello im an Agent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(1f*Time.deltaTime, 0f, 0f);
+    }
+}
