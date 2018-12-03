b86f6bfb46bf5f6dfb49f16453c70fe894982fdb
OvanHouten <olaf.vanhouten@student.uva.nl>
2018-12-03 03:28:34 +0100

    Added breadcrumb and camera movement

---
+++ Colony Behavior/Assets/Scripts/VoxelCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+		for (int i = -5; i < 10; i++) {
+			for (int j = -5; j < 10; j++) {
+				for (int k= -5; k < 10; k++) {
+					VoxelTools.MakeCube(i, j, k);
+				}
+			}
+		}
+        
+    }
+}
