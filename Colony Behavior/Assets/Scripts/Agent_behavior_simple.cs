384d153deb9605bb0e1957d300b4c4f9ec042a36
OvanHouten <olaf.vanhouten@student.uva.nl>
2018-12-04 04:01:58 +0100

    Added a sensing ability to th agent

---
+++ Colony Behavior/Assets/Scripts/Agent_behavior_simple.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent_behavior_simple : MonoBehaviour
+{
+	Vector3 movement;
+	
+	private void Start() {
+		movement = new Vector3(1f * Time.deltaTime, 0f, 0f);
+		print("sldnfs");
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		transform.Translate(movement);
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		print("kjf");
+		movement = -movement;
+	}
+
+}
