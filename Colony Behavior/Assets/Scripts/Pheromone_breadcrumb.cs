48c7b42481d0d66c17f0f44e50420c6919d91465
OvanHouten <olaf.vanhouten@student.uva.nl>
2019-02-06 23:26:03 +0100

    Added Slider for later realtime Settings

--- Colony Behavior/Assets/Scripts/Pheromone_breadcrumb.cs
+++ Colony Behavior/Assets/Scripts/Pheromone_breadcrumb.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 
 public class Pheromone_breadcrumb : MonoBehaviour
 {
-	int value;
+	MeshRenderer meshRenderer;
+	float value;
+	float evaporation_rate;
 
 	// Start is called before the first frame update
     void Start()
     {
-		value = 0;
-		//print("crumb = " + value);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+		value = 0f;
+		evaporation_rate = 0.1f;
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.material.SetColor("_Color", new Vector4(0.0f, 0.0f, 0.0f, 0.0f));
 	}
 
-	private void OnTriggerEnter(Collider other) {
+	// Update is called once per frame
+	void Update() {
+		float a_value = (value / 100f);
+		float rgb_value = 1 - a_value;
+		print("rgb = " + rgb_value);
+		print("a = " + a_value);
+		meshRenderer.material.SetColor("_Color", new Vector4(0.0f, 0.0f, 0.0f, a_value));
+		//meshRenderer.material.SetColor("_Color", new Vector4(rgb_value, rgb_value, rgb_value, a_value));
+
+		value -= evaporation_rate;
+		if (value < 0.0f) {
+			value = 0.0f;
+		}
+	}
+
+	private void OnTriggerStay(Collider other) {
 		value = value + 1;
-		//print("crumb = " + value);
+
+		if (value > 100.0f) {
+			value = 100.0f;
+		}
+	}
+
+	//Test function, remove later with button
+	public void AddPheromones() {
+		value += 10;
+		if (value > 100) {
+			value = 100;
+		}
 	}
 }
--- Colony Behavior/Assets/Scripts/SpawnPheromones.cs
+++ Colony Behavior/Assets/Scripts/SpawnPheromones.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VoxelCube : MonoBehaviour
+public class SpawnPheromones : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start() {
 		for (int i = -5; i < 10; i++) {
 			for (int j = -5; j < 10; j++) {
 				for (int k= -5; k < 10; k++) {
-					VoxelTools.MakeCube(i, j, k);
+					PheromoneHelper.MakePheromone(i, j, k);
 				}
 			}
 		}
